0a033ed048ae91cb18ed776d16cb286a2ba07fef
HWliao <499606788@qq.com>
2017-06-27 19:31:30 +0800

    clipboard init

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+var it = require("mocha").it;
+var describe = require("mocha").describe;
+var expect = require('chai').expect;
+var debug = require('debug')('clipboard');
+
+debug('safasfasfasfasfs');
+
+describe('Test mocha', function () {
+  it('test lhw', function () {
+    debug('safasfasfasfasfs');
+    expect('abc').to.be.a('string');
+  });
+});
